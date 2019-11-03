0302a14f5476a69b8f8e30fe4d7cf1b021e57e15
sami016 <samholder@live.co.uk>
2019-11-03 14:14:25 +0000

    added missing OtherScene class.

---
+++ Samples/SampleGame/OtherScene.cs
@@ -0,0 +1,14 @@
+﻿using Forge.Core.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleGame
+{
+    public class OtherScene : Scene
+    {
+        public override void Initialise()
+        {
+        }
+    }
+}
