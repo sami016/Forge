3757e1c003d77e73d5548b313b66ee6ca393f8eb
sami016 <samholder@live.co.uk>
2020-04-22 19:35:31 +0100

    Added additional comment.

--- Libraries/Forge.Core/Space/ISpace.cs
+++ Libraries/Forge.Core/Space/ISpace.cs
@@ -8,6 +8,12 @@ namespace Forge.Core.Space
 {
     public interface ISpace
     {
+        /// <summary>
+        /// Get nearby entities.
+        /// </summary>
+        /// <param name="location">location</param>
+        /// <param name="radius">radius in which to search</param>
+        /// <returns></returns>
         IEnumerable<Entity> GetNearby(Vector3 location, float radius);
 
         /// <summary>
